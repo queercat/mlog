c193fa90cc338fb824de68e324826ca110c7e5ec
queercat <tusek.may@gmail.com>
2021-05-26 09:11:59 -0700

    That hash has been mapped.

--- include/generator.h
+++ include/generator.h
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 #include <assert.h>
 
@@ -9,9 +10,12 @@ class Generator {
 	private:
 		std::string blog_location;
 		std::string config_location;
+		std::unordered_map<std::string, std::string> config;
 
 		std::fstream open_file(const std::string&);
+
 		void read_config(std::fstream*, std::vector<std::string>*);
+		void parse_config(std::vector<std::string>*);
 
 	public:
 		void generate_blog(const std::string&);
@@ -27,7 +31,7 @@ std::fstream Generator::open_file(const std::string& file_location) {
 
 /**
  * @desc read_config : reads a file pointer's arguments and puts it into an arguments vector.
- * See "doc/reference_config.mconfig" for an example.
+ * @note See "doc/reference_config.mconfig" for an example.
  */
 void Generator::read_config(std::fstream* config_file, std::vector<std::string>* arguments) {
 	std::string line;
@@ -56,8 +60,28 @@ void Generator::read_config(std::fstream* config_file, std::vector<std::string>*
 		}
 
 		if (is_valid) {
-			std::cout << line.substr(positions[0] + 1, positions[2] - 1) << std::endl;
+			arguments->push_back(line.substr(positions[0] + 1, positions[2] - 1));
 		}
+	}
+}
+
+/**
+ * @desc parse_config : Reads the config vector into a umap.
+ */
+void Generator::parse_config(std::vector<std::string>* config_arguments) {
+	std::string argument;
+
+	while (!config_arguments->empty()) {
+		argument = config_arguments->back();
+		config_arguments->pop_back();
+
+		// Split the string doing some substr funstuffs.
+		int seperator_position = (int)argument.find(":");
+
+		std::string key = argument.substr(0, seperator_position);
+		std::string val = argument.substr(seperator_position + 1, argument.length());
+
+		this->config[key] = val;
 	}
 }
 
@@ -71,7 +95,16 @@ void Generator::generate_blog(const std::string& blog_location) {
 
 	assert(config_file.is_open());
 
-	std::vector<std::string> arguments;
+	std::vector<std::string> config_arguments;
 
-	this->read_config(&config_file, &arguments);
+	this->read_config(&config_file, &config_arguments);
+
+	config_file.close();
+
+	assert(!config_arguments.empty());
+
+	this->parse_config(&config_arguments);
+
+	assert(config_arguments.empty());
+	assert(!this->config.empty());
 }
