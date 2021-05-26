26ed3cb8da7c1dda08f575a742c0c11047d0544c
queercat <tusek.may@gmail.com>
2021-05-26 08:40:27 -0700

    File parsing to vector works.

--- include/generator.h
+++ include/generator.h
@@ -32,8 +32,32 @@ std::fstream Generator::open_file(const std::string& file_location) {
 void Generator::read_config(std::fstream* config_file, std::vector<std::string>* arguments) {
 	std::string line;
 
+	std::string l_bracket = "{";
+	std::string colon = ":";
+	std::string r_bracket = "}";
+
+	std::string valid_params[3] = {l_bracket, colon, r_bracket};
+
 	while (std::getline(*config_file, line)) {
-		std::cout << line << std::endl;
+		// Check if the line is a valid instruction. Will contain [:].
+		bool is_valid = true; 
+
+		int positions[3]; // Same order as valid_params.
+
+		for (int i = 0; i < 3; i++) {
+			std::size_t found = line.find(valid_params[i]);
+
+			if (found == std::string::npos) {
+				is_valid = false;
+				break;
+			}
+
+			positions[i] = (int)found;
+		}
+
+		if (is_valid) {
+			std::cout << line.substr(positions[0] + 1, positions[2] - 1) << std::endl;
+		}
 	}
 }
 
